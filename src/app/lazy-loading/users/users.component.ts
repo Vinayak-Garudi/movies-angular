9c97980390d066d9105d8c7995549952d0d05bb7
Vinayak-Garudi <hawkigarudi@gmail.com>
2023-05-17 13:06:58 +0530

    guage chart using apex charts

--- src/app/lazy-loading/users/users.component.ts
+++ src/app/lazy-loading/users/users.component.ts
@@ -1,19 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserDetailsService } from 'src/app/services/user-details.service';
-// import {
-//   ChartComponent,
-//   ApexAxisChartSeries,
-//   ApexChart,
-//   ApexXAxis,
-//   ApexTitleSubtitle
-// } from "ng-apexcharts";
-
-// export type ChartOptions = {
-//   series: ApexAxisChartSeries;
-//   chart: ApexChart;
-//   xaxis: ApexXAxis;
-//   title: ApexTitleSubtitle;
-// };
 
 @Component({
   selector: 'app-users',
@@ -25,9 +11,6 @@ import { UserDetailsService } from 'src/app/services/user-details.service';
 
 export class UsersComponent implements OnInit {
 
-  // @ViewChild("chart") chart!: ChartComponent;
-
-  // public chartOptions: Partial<ChartOptions>;
 
   chartSeries: any = [40, 32, 28, 55]
   chartDetails: any = {
@@ -70,25 +53,31 @@ export class UsersComponent implements OnInit {
     }
   ];
 
+  public guageChartOptions: any
+
+
   constructor(private userData: UserDetailsService) {
-    // this.chartOptions = {
-    //   series: [
-    //     {
-    //       name: "My-series",
-    //       data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-    //     }
-    //   ],
-    //   chart: {
-    //     height: 350,
-    //     type: "bar"
-    //   },
-    //   title: {
-    //     text: "My First Angular Chart"
-    //   },
-    //   xaxis: {
-    //     categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
-    //   }
-    // };
+    this.guageChartOptions = {
+      series: [75],
+      chart: {
+        type: 'radialBar'
+      },
+      plotOptions: {
+        radialBar: {
+          hollow: {
+            size: '50%'
+          },
+          track: {
+            background: "#fff",
+          },
+          fill: {
+            colors: ['#aa4a44']
+          }
+        },
+        labels: ['Guage'],
+      },
+    }
+
   }
 
   ngOnInit(): void {
