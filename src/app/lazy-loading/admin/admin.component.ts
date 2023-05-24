d6edb596206bc52c5862f01f31fc39420e5bb7c3
Vinayak-Garudi <hawkigarudi@gmail.com>
2023-05-24 12:27:58 +0530

    implemented and understood custom validators

--- src/app/lazy-loading/admin/admin.component.ts
+++ src/app/lazy-loading/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -7,14 +7,25 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+
+  rangeValidator(min: number, max: number) {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      const value = control.value
+      if (value && (value.length > max || value.length < min)) {
+        return { range: true }
+      }
+      return null
+    }
+  }
+
   adminForm: FormGroup = this.fb.group({
     username: [''],
-    fname: [''],
+    fname: ['', [this.rangeValidator(5, 10), Validators.required]],
     email: ['', [Validators.required, Validators.minLength(5), Validators.email, Validators.maxLength(15)]],
     contact: ['']
   })
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) { }
 
   get emailCheck() {
     return this.adminForm.get('email')
