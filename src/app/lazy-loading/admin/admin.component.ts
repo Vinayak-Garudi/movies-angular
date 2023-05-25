0ad968fd7ff548668336066a886873f0ee2fb3c8
Vinayak-Garudi <hawkigarudi@gmail.com>
2023-05-25 12:44:33 +0530

    different arrays for different ip names using formarray

--- src/app/lazy-loading/admin/admin.component.ts
+++ src/app/lazy-loading/admin/admin.component.ts
@@ -1,72 +1,126 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl, FormArray } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  FormArray,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
-
 export class AdminComponent implements OnInit {
-
   myForm: FormGroup;
-  adminForm: FormGroup
-  dummyArr: any[] = [1, 2]
-  objArr: any[] = [
-    { res1: "res1" },
-    { res2: "res2" },
-    { res3: "res3" },
-  ]
+  adminForm: FormGroup;
+  newForm: FormGroup;
+  dummyArr: any[] = [1, 2];
+  auxEngArr: any[] = [1, 2, 3];
+  mainEngArr: any[] = [1, 2, 3, 4];
+  objArr: any[] = [{ res1: 'res1' }, { res2: 'res2' }, { res3: 'res3' }];
   dummyObj: any = {
-    data1: "data1",
-    data2: "data2",
-    data3: "data3"
-  }
+    data1: 'data1',
+    data2: 'data2',
+    data3: 'data3',
+  };
 
   rangeValidator(min: number, max: number) {
     return (control: AbstractControl): { [key: string]: boolean } | null => {
-      const value = control.value
+      const value = control.value;
       if (value && (value.length > max || value.length < min)) {
-        return { range: true }
+        return { range: true };
       }
-      return null
-    }
+      return null;
+    };
   }
 
   get itemControls() {
     return this.myForm.get('items') as FormArray;
   }
 
+  get newItemControls() {
+    return this.newForm.get('newItems') as FormArray;
+  }
+
   constructor(private fb: FormBuilder) {
+    this.newForm = this.fb.group({
+      newItems: this.fb.array([]),
+    });
+
     this.myForm = this.fb.group({
       items: this.fb.array([]),
-      dummyip: ['']
+      dummyip: [''],
     });
 
     this.adminForm = this.fb.group({
       username: [''],
       fname: ['', [this.rangeValidator(5, 10), Validators.required]],
-      email: ['', [Validators.required, Validators.minLength(5), Validators.email, Validators.maxLength(15)]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.email,
+          Validators.maxLength(15),
+        ],
+      ],
       contact: [''],
-      arr: this.myForm
-    })
+      arr: this.myForm,
+    });
   }
 
   ngOnInit(): void {
     this.dummyArr.forEach(() => {
       this.itemControls.push(this.createItem());
-    })
-    this.objArr.forEach(ele => {
-      Object.assign(this.dummyObj, ele)
-    })
-    delete this.dummyObj['res1']
-    console.log("objjjj", this.dummyObj)
+    });
+    this.objArr.forEach((ele) => {
+      Object.assign(this.dummyObj, ele);
+    });
+    delete this.dummyObj['res1'];
+    console.log('objjjj', this.dummyObj);
+
+    this.auxEngArr.forEach(() => {
+      this.addNewItem('auxEng');
+    });
+
+    this.mainEngArr.forEach(() => {
+      this.addNewItem('mainEng');
+    });
   }
 
   createItem(): FormGroup {
     return this.fb.group({
       name: ['', Validators.required],
     });
+  }
+
+  createNewItem(): FormGroup {
+    return this.fb.group({
+      auxEng: ['', Validators.required],
+      mainEng: ['', Validators.required],
+    });
+  }
+
+  addNewItem(controlName: string) {
+    if (controlName === 'auxEng') {
+      const temp = this.createNewItem();
+      temp.removeControl('mainEng');
+      this.newItemControls.push(temp);
+    } else if (controlName === 'mainEng') {
+      const temp = this.createNewItem();
+      temp.removeControl('auxEng');
+      this.newItemControls.push(temp);
+    }
+  }
+
+  auxEngAdded(index: number) {
+    return this.newItemControls.at(index).get('auxEng') !== null;
+  }
+
+  mainEngAdded(index: number) {
+    return this.newItemControls.at(index).get('mainEng') !== null;
   }
 
   addItem() {
@@ -78,10 +132,10 @@ export class AdminComponent implements OnInit {
   }
 
   showData() {
-    console.log("dataaa", this.myForm.value, this.adminForm.value)
+    console.log('dataaa', this.myForm.value, this.adminForm.value);
   }
 
   get emailCheck() {
-    return this.adminForm.get('email')
+    return this.adminForm.get('email');
   }
 }
--- src/app/lazy-loading/chat-gpt/chat-gpt.component.ts
+++ src/app/lazy-loading/chat-gpt/chat-gpt.component.ts
@@ -6,38 +6,45 @@ import { UserDetailsService } from 'src/app/services/user-details.service';
 @Component({
   selector: 'app-chat-gpt',
   templateUrl: './chat-gpt.component.html',
-  styleUrls: ['./chat-gpt.component.css']
+  styleUrls: ['./chat-gpt.component.css'],
 })
 export class ChatGptComponent {
-  chatResponses: any = []
+  chatResponses: any = [];
   openAIForm: FormGroup = this.fb.group({
-    promptValue: ['', Validators.required]
-  })
+    promptValue: ['', Validators.required],
+  });
 
   // learn ng-zorro and scss
 
-  constructor(private http: HttpClient, private userDetails: UserDetailsService, private fb: FormBuilder) { }
+  constructor(
+    private http: HttpClient,
+    private userDetails: UserDetailsService,
+    private fb: FormBuilder
+  ) {}
 
   handleSend() {
     if (this.openAIForm.get('promptValue')?.value.length > 0) {
       const headers = new HttpHeaders()
         .set('content-type', 'application/json')
         .set('Type', 'chatgpt4')
-        .set('X-RapidAPI-Key', '7d24bdaad4msh8f98b48025207abp1cb766jsncbe35c377280')
-        .set('X-RapidAPI-Host', 'chatgpt-open-ai-nlp.p.rapidapi.com')
+        .set(
+          'X-RapidAPI-Key',
+          '7d24bdaad4msh8f98b48025207abp1cb766jsncbe35c377280'
+        )
+        .set('X-RapidAPI-Host', 'chatgpt-open-ai-nlp.p.rapidapi.com');
       const body = {
         prompt: JSON.stringify(this.openAIForm.get('promptValue')?.value),
-        temperature: '0.7'
-      }
-      this.http.post<any[]>(this.userDetails.openAIApi, body, { headers }).subscribe((res: any) => {
-        this.chatResponses.push(
-          {
+        temperature: '0.7',
+      };
+      this.http
+        .post<any[]>(this.userDetails.openAIApi, body, { headers })
+        .subscribe((res: any) => {
+          this.chatResponses.push({
             question: this.openAIForm.get('promptValue')?.value,
-            answer: res.answer
-          }
-        )
-        this.openAIForm.reset()
-      })
+            answer: res.answer,
+          });
+          this.openAIForm.reset();
+        });
     }
   }
 }
